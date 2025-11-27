90c398b726125cf65bdec3dc92f780b15a1d3b9e
gitdashboardonline <starapps9347@gmail.com>
2025-11-27 16:57:05 +0530

    end of section 15

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -38,7 +38,7 @@ public class AccountController(SignInManager<AppUser> signInManager) : BaseApiCo
 
 
     [Authorize]
-    [HttpPost]
+    [HttpPost("logout")]
     public async Task<ActionResult> Logout()
     {
         await signInManager.SignOutAsync();
@@ -65,7 +65,7 @@ public class AccountController(SignInManager<AppUser> signInManager) : BaseApiCo
         });
     }
 
-    [HttpGet]
+    [HttpGet("auth-status")]
     public ActionResult GetAuthState()
     {
         return Ok(new { IsAuthenticated = User.Identity?.IsAuthenticated ?? false });
